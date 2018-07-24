ca96236831cc0bc3176be724198db43c4528d71e
Christoph Portmann <portmann.christoph@gmail.com>
2018-07-24 14:28:02 +0200

    monitor internet connection with simple GET request

---
+++ internetmonitor/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"time"
+	"github.com/bugsnag/bugsnag-go/errors"
+)
+
+var timeFormat = "15:04:05"
+
+func main() {
+	lastOnline := time.Now()
+	fmt.Printf("time\t\tstatus\tlast\ttotal\n")
+	totalOfflineTime := time.Second * 0
+	for {
+		t := time.Now()
+		err := testConnection()
+		stat := "offline"
+		if err == nil {
+			stat = "online"
+			lastOnline = t
+		}
+		fmt.Printf("%v\t%v\t%vs\t%v\n", t.Format(timeFormat), stat, t.Sub(lastOnline).Truncate(time.Second).Seconds(), totalOfflineTime)
+		if err !=nil {
+			totalOfflineTime = totalOfflineTime + 5*time.Second
+		}
+		time.Sleep(5 * time.Second)
+	}
+}
+
+func testConnection() error {
+	req, err := http.NewRequest("GET", "http://www.google.com", nil)
+	if err != nil {
+		return err
+	}
+	cli := http.Client{Timeout: 4 * time.Second}
+	res, err := cli.Do(req)
+	if err != nil {
+		return err
+	}
+	if res.StatusCode != 200 {
+		return errors.Errorf("not 200 %v", res)
+	}
+	return nil
+}
